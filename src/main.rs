ec5a6aca415151df33ce6b59582e1d1ab1309297
Rachel Mathilda <rachel.mathilda@ui.ac.id>
2025-03-18 19:56:14 +0700

    (3) Validating request and selectively responding

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@ use std::{
 	io::{prelude::*, BufReader},     
 	net::{TcpListener, TcpStream}, 
 }; 
-// --snip--  
+ 
 fn main() {
 	let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
 
@@ -16,19 +16,27 @@ fn main() {
 
 fn handle_connection(mut stream: TcpStream) {     
 	let buf_reader = BufReader::new(&mut stream);     
-	let http_request: Vec<_> = buf_reader         
-		.lines()         
-		.map(|result| result.unwrap())         
-		.take_while(|line| !line.is_empty())         
-		.collect();   
+	let request_line = buf_reader.lines().next().unwrap().unwrap();
 
-	let status_line = "HTTP/1.1 200 OK";     
-	let contents = fs::read_to_string("hello.html").unwrap();     
-	let length = contents.len();    
+	if request_line == "GET / HTTP/1.1" {
+		let status_line = "HTTP/1.1 200 OK";     
+		let contents = fs::read_to_string("hello.html").unwrap();     
+		let length = contents.len();    
+	
+		let response =         
+			format!("{status_line}\r\nContent-Length: 
+	{length}\r\n\r\n{contents}");
+	
+		stream.write_all(response.as_bytes()).unwrap();
+	} else {
+		let status_line = "HTTP/1.1 404 NOT FOUND";
+        let contents = fs::read_to_string("404.html").unwrap();
+        let length = contents.len();
 
-	let response =         
-        format!("{status_line}\r\nContent-Length: 
-{length}\r\n\r\n{contents}");
+        let response = format!(
+            "{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}"
+        );
 
-    stream.write_all(response.as_bytes()).unwrap(); 
+        stream.write_all(response.as_bytes()).unwrap();
+	}
 } 
