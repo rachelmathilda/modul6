2298cdf71c14515f6f9198f4c09337f63763f96f
Rachel Mathilda <rachel.mathilda@ui.ac.id>
2025-03-19 12:02:40 +0700

    (4) Corrected Simulation of slow request.

--- src/main.rs
+++ src/main.rs
@@ -28,4 +28,12 @@ fn handle_connection(mut stream: TcpStream) {
 		}
 		_ => ("HTTP/1.1 404 NOT FOUND", "404.html"),
 	};
+
+	let contents = fs::read_to_string(filename).unwrap();
+    let length = contents.len();
+
+	let response = format!(
+"{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}");
+
+	stream.write_all(response.as_bytes()).unwrap();
 } 
